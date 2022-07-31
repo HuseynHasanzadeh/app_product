cfb159f73abe049196e08d8b387e9d72f7fbf881
hhasanzade <hasanzade.huseyn6@student.ucode.world>
2022-07-31 07:54:44 +0300

    finish change

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".fa-bars").click(function() {
+        $('.menu__aside').addClass("menu__active");
+        
+    $('.close__btn').click(function(){
+        $('.menu__aside').removeClass('menu__active')
+    });
+
+});
+
+
+});
+
